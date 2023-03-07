777d2875d5211f0ee4383163c465400411f07201
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-03-07 13:13:09 +0300

    Added structure and functionality

---
+++ src/display.js
@@ -0,0 +1,12 @@
+const generatedInput = document.querySelector('.generated-input');
+const inputName = document.querySelector('.input-name');
+const inputScore = document.querySelector('.input-score');
+const submitBtn = document.querySelector('.submit-btn');
+
+export const displayScore = () => {
+  const displayName = document.createElement('p');
+  generatedInput.appendChild(displayName);
+  displayName.textContent = `${inputName.value} : ${inputScore.value} `;
+  inputName.value = '';
+  inputScore.value = '';
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import './style.css';
+import * as display from './display.js';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+
+const submitBtn = document.querySelector('.submit-btn');
+
+submitBtn.addEventListener('click', () => {
+  display.displayScore();
+})
