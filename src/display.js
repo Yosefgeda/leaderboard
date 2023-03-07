f8ad1e4608848daa391f864fb31f067f932c5715
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-03-07 16:13:36 +0300

    update display function

--- src/display.js
+++ src/display.js
@@ -1,7 +1,7 @@
 const generatedInput = document.querySelector('.generated-input');
 const inputName = document.querySelector('.input-name');
 const inputScore = document.querySelector('.input-score');
-const submitBtn = document.querySelector('.submit-btn');
+
 
 export const displayScore = () => {
   const displayName = document.createElement('p');
@@ -9,4 +9,4 @@ export const displayScore = () => {
   displayName.textContent = `${inputName.value} : ${inputScore.value} `;
   inputName.value = '';
   inputScore.value = '';
-};
+}
